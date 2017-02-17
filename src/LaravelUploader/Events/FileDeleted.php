e57b911a07cf2ece9f933b742d4009b30363f820
jcc <changejian@gmail.com>
2017-02-17 11:14:42 +0800

    Updated the file's events

--- src/LaravelUploader/Events/FileDeleted.php
+++ src/LaravelUploader/Events/FileDeleted.php
@@ -8,15 +8,19 @@
 
 namespace Overtrue\LaravelUploader\Events;
 
+use Illuminate\Http\UploadedFile;
+
 class FileDeleted
 {
     public $file;
 
     /**
      * Create a new event instance.
+     * 
+     * @param Illuminate\Http\UploadedFile $file
      */
-    public function __construct()
+    public function __construct(UploadedFile $file)
     {
-        //
+        $this->file = $file;
     }
 }
