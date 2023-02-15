8f12be9f8361b80161ddc94668f8d8af0d2ec2f8
overtrue <anzhengchao@gmail.com>
2023-02-15 15:36:44 +0800

    Laravel 10

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,13 +1,4 @@
 <?php
 
-/*
- * This file is part of the overtrue/laravel-uploader.
- *
- * (c) overtrue <i@overtrue.me>
- *
- * This source file is subject to the MIT license that is bundled
- * with this source code in the file LICENSE.
- */
-
 include __DIR__.'/../vendor/autoload.php';
 include __DIR__.'/TestCase.php';
